6b318a67e899e770d59cd7ac37c5f27fe5a80c2a
mohsenpiri61 <mopiry@gmail.com>
2024-06-23 17:52:24 +0330

    api

--- core/core/urls.py
+++ core/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/',include('todo.urls')),
-    path('users/',include('users.urls'))
+    path('tasks/', include('todo.urls')),
+    path('users/', include('users.urls')),
 ]
--- core/users/urls.py
+++ core/users/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
-from django.contrib.auth.views import LogoutView
-from .views import MyLoginView, RegisterView
- 
+from django.urls import path, include
+from . import views
+
+
 
 urlpatterns = [
-    path('login/', MyLoginView.as_view(),name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'),name='logout'),
-    path('register/', RegisterView.as_view(),name='register'),
+    path("api/", include("users.api.urls")),
+    path("login/", views.indexView, name="authentication"),
 ]
