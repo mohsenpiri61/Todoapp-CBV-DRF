f20fb4b1e1086284e2d30e8d71c55baf3882e205
mohsenpiri61 <mopiry@gmail.com>
2024-08-08 19:06:27 +0330

    amned

--- core/weather/models.py
+++ core/weather/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Weather(models.Model):
-    lantitude = models.FloatField(max_value=100)
-    longtitude = models.FloatField(max_value=100)
+    lantitude = models.FloatField(null=True)
+    longtitude = models.FloatField(null=True)
     temperature = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
